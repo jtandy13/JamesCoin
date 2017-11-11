a5aa87f2e1d4461dc24b06b6ebe9c2fc102daa13
james.tandy <james.tandy@servicenow.com>
2017-11-11 19:35:24 +1100

    commented the nonce value

--- src/block.js
+++ src/block.js
@@ -12,6 +12,7 @@ class Block {
 
   calculateHash() {
     //encrypt the entire contents of the block
+    //without the nonce value, the hash will always be the same as the other values in the block do not change.
     return SHA256(this.index + this.previousHash + this.UTCtimestamp + JSON.stringify(this.data) + this.nonce).toString();
   }
 
@@ -21,6 +22,10 @@ class Block {
      * add one to the nonce and hash again.
      */
     while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      /**
+       * If the current hash does not have the correct number of "0"s at the start,
+       * increase the nonce by 1 and try again.
+       */
       this.nonce++;
       this.hash = this.calculateHash();
     }
